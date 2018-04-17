f96834f837a448ff6c6a4ebaf60009f37db4ef9d
dtesfai <dmtesfai@uwaterloo.ca>
2018-04-17 18:12:31 -0400

    Replaced nav dropdown with horizontal menu

--- js/home.js
+++ js/home.js
@@ -1,38 +1,23 @@
-// function toggleNav() {
-//         var nav = document.querySelector('.navbar-menu');
-//         if(nav.className == "navbar-menu") {
-//             nav.className = "navbar-menu is-active";
-//         } else {
-//             nav.className = "navbar-menu";
-//         }
-// }
+// document.addEventListener("DOMContentLoaded", function() {
+//   // Get all "navbar-burger" elements
+//   var $navbarBurgers = Array.prototype.slice.call(
+//     document.querySelectorAll(".navbar-burger"),
+//     0
+//   );
 
-// function hamburgerHelper(){
-//     document.querySelector('.navbar-burger').addEventListener("click", toggleNav);
-// }
+//   // Check if there are any navbar burgers
+//   if ($navbarBurgers.length > 0) {
+//     // Add a click event on each of them
+//     $navbarBurgers.forEach(function($el) {
+//       $el.addEventListener("click", function() {
+//         // Get the target from the "data-target" attribute
+//         var target = $el.dataset.target;
+//         var $target = document.getElementById(target);
 
-document.addEventListener('DOMContentLoaded', function () {
-
-    // Get all "navbar-burger" elements
-    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-  
-      // Add a click event on each of them
-      $navbarBurgers.forEach(function ($el) {
-        $el.addEventListener('click', function () {
-  
-          // Get the target from the "data-target" attribute
-          var target = $el.dataset.target;
-          var $target = document.getElementById(target);
-  
-          // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-          $el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
-  
-        });
-      });
-    }
-  
-  });
+//         // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+//         $el.classList.toggle("is-active");
+//         $target.classList.toggle("is-active");
+//       });
+//     });
+//   }
+// });
